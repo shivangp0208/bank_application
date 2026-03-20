2fb4dbe13b436c4aebda9cdd4436a1012a2df83c
Shivang Patel <patelshivang702@gmail.com>
2026-03-20 11:02:43 +0530

    initialized a go base bank application project

---
+++ db/schema/BankSchema.sql
@@ -0,0 +1,44 @@
+-- =========================
+-- accounts
+-- =========================
+CREATE TABLE IF NOT EXISTS accounts (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  owner VARCHAR(255) NOT NULL,
+  balance BIGINT NOT NULL,
+  currency VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_accounts_owner ON accounts(owner);
+
+-- =========================
+-- entries
+-- =========================
+CREATE TABLE IF NOT EXISTS entries (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  account_id BIGINT UNSIGNED,
+  amount BIGINT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+CREATE INDEX idx_entries_account_id ON entries(account_id);
+
+-- =========================
+-- transfers
+-- =========================
+CREATE TABLE IF NOT EXISTS transfers (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  from_account_id BIGINT UNSIGNED NOT NULL,
+  to_account_id BIGINT UNSIGNED NOT NULL,
+  amount BIGINT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  FOREIGN KEY (from_account_id) REFERENCES accounts(id),
+  FOREIGN KEY (to_account_id) REFERENCES accounts(id)
+);
+
+CREATE INDEX idx_transfers_from_account ON transfers(from_account_id);
+CREATE INDEX idx_transfers_to_account ON transfers(to_account_id);
+CREATE INDEX idx_transfers_from_to ON transfers(from_account_id, to_account_id);
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shivangp0208/bank_application
+
+go 1.26.1
